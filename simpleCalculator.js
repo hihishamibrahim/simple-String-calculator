8971e1e6f4d721b366e4b3e24222b44474bda543
Hisham Ibrahim <m.ibrahim@settlin.in>
2025-04-09 21:35:56 +0530

    adds default delimiter

--- simpleCalculator.js
+++ simpleCalculator.js
@@ -1,15 +1,25 @@
 function add (numbers) {
     if (!numbers) return 0;
 
-    //check next line and replace with comma
-    const numbersStr = numbers.replace(/\n/g, ',');
+    let delimiter = '';
     
-    const negativeNumbers = numbersStr.split(',').filter(num => parseInt(num,10) < 0);
+    // Handle delimiter
+    if (numbers.startsWith('//')) {
+        numbers = numbers.substring(2);
+        delimiter = '[\n;]'
+    } else delimiter = '\n'
+
+    numbers = numbers.replace(`/${delimiter}/g`, ',');
+
+    // Handle empty strings and converts string to number
+    numArray = numbers.split(',').map(n => parseInt(n, 10)||0);
+    
+    // handle negative numbers
+    const negativeNumbers = numArray.filter(num => parseInt(num,10) < 0);
     if (negativeNumbers.length > 0) throw new Error('Negative numbers are not allowed ' + negativeNumbers.join(','));
     
-    // Split by comma and convert to numbers
-    const allNumbers = numbersStr.split(',').map(num => parseInt(num, 10));
-    sum = allNumbers.reduce((a,b)=> b>1000? a : a + b,0)
+    // Find sum of all numbers in a  array
+    sum = numArray.reduce((a,b)=> b>1000? a : a + b,0)
     return sum;
 }
 
