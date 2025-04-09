e0cf19d8c72bafca0ae56b2081c4f888ec3e15eb
Hisham Ibrahim <m.ibrahim@settlin.in>
2025-04-09 08:58:14 +0530

    add checks for negative numbers and throw a error with all the negative numbers

--- simpleCalculator.js
+++ simpleCalculator.js
@@ -1,7 +1,10 @@
 function add (numbers) {
     if (!numbers) return 0;
-
-    sum = numbers.split(',').reduce((a,b)=>parseInt(a)+parseInt(b),0)
+    const negativeNumbers = numbers.split(',').filter(num => parseInt(num) < 0);
+    if (negativeNumbers.length > 0) {
+        throw new Error('Negative numbers are not allowed ' + negativeNumbers.join(','));
+    }
+    sum = numbers.split(',').reduce((a,b)=> parseInt(a)+parseInt(b),0)
     return sum;
 }
 
