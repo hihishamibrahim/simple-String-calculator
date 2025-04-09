0e6f9ba234ad600abb9235c606dbd608135764d9
Hisham Ibrahim <m.ibrahim@settlin.in>
2025-04-09 20:10:56 +0530

    adds check to ignore number above 1000

--- simpleCalculator.js
+++ simpleCalculator.js
@@ -1,15 +1,15 @@
 function add (numbers) {
     if (!numbers) return 0;
 
-    //checl next line and replace with comma
+    //check next line and replace with comma
     const numbersStr = numbers.replace(/\n/g, ',');
-  
+    
     const negativeNumbers = numbersStr.split(',').filter(num => parseInt(num,10) < 0);
     if (negativeNumbers.length > 0) throw new Error('Negative numbers are not allowed ' + negativeNumbers.join(','));
     
     // Split by comma and convert to numbers
     const allNumbers = numbersStr.split(',').map(num => parseInt(num, 10));
-    sum = allNumbers.reduce((a,b)=> a + b,0)
+    sum = allNumbers.reduce((a,b)=> b>1000? a : a + b,0)
     return sum;
 }
 
