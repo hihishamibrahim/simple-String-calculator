8eff5792e720cf96884a1bb123206ab1a482d96c
Hisham Ibrahim <m.ibrahim@settlin.in>
2025-04-09 08:43:31 +0530

    add test cases for single number and addition of multiple numbers seperated by coma ,

--- simpleCalculator.test.js
+++ simpleCalculator.test.js
@@ -1,10 +1,16 @@
 const SimpleCalculator = require('./simpleCalculator');
 
 describe('SimpleCalculator', () => {
-    it('should add two numbers', () => {
+    it('should add numbers seperated by coma', () => {
         expect(SimpleCalculator('1,2')).toBe(3);
+        expect(SimpleCalculator('1,2,10')).toBe(13);
+        expect(SimpleCalculator('10,2,1,100')).toBe(113);
     });
     it('should return 0 if empty string', () => {
         expect(SimpleCalculator('')).toBe(0);
+    });
+    it('test single number', () => {
+        expect(SimpleCalculator('5')).toBe(5);
+        expect(SimpleCalculator('15')).toBe(15);
     });
 });
