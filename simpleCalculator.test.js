e3afc7850e4b2cc9b1adc43ae03ae0aea83dbece
Hisham Ibrahim <m.ibrahim@settlin.in>
2025-04-09 08:58:58 +0530

    add test cases for single and multiple  negative numbers seperated by ,

--- simpleCalculator.test.js
+++ simpleCalculator.test.js
@@ -13,4 +13,10 @@ describe('SimpleCalculator', () => {
         expect(SimpleCalculator('5')).toBe(5);
         expect(SimpleCalculator('15')).toBe(15);
     });
+    it('test for negative numbers', () => {
+        expect(()=>SimpleCalculator('-5')).toThrow('Negative numbers are not allowed -5');
+        expect(()=>SimpleCalculator('-5,-11')).toThrow('Negative numbers are not allowed -5,-11');
+        expect(()=>SimpleCalculator('-5,-15,5')).toThrow('Negative numbers are not allowed -5,-15');
+        expect(()=>SimpleCalculator('-5,-10,5,10')).toThrow('Negative numbers are not allowed -5,-10');
+    });
 });
