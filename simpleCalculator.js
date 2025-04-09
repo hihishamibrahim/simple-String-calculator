2803f562ee22eba60496e49b6c2990dfe03a9b41
Hisham Ibrahim <m.ibrahim@settlin.in>
2025-04-09 09:31:13 +0530

    add numbers if there is a next line

--- simpleCalculator.js
+++ simpleCalculator.js
@@ -1,10 +1,15 @@
 function add (numbers) {
     if (!numbers) return 0;
-    const negativeNumbers = numbers.split(',').filter(num => parseInt(num) < 0);
-    if (negativeNumbers.length > 0) {
-        throw new Error('Negative numbers are not allowed ' + negativeNumbers.join(','));
-    }
-    sum = numbers.split(',').reduce((a,b)=> parseInt(a)+parseInt(b),0)
+
+    //checl next line and replace with comma
+    const numbersStr = numbers.replace(/\n/g, ',');
+  
+    const negativeNumbers = numbersStr.split(',').filter(num => parseInt(num,10) < 0);
+    if (negativeNumbers.length > 0) throw new Error('Negative numbers are not allowed ' + negativeNumbers.join(','));
+    
+    // Split by comma and convert to numbers
+    const allNumbers = numbersStr.split(',').map(num => parseInt(num, 10));
+    sum = allNumbers.reduce((a,b)=> a + b,0)
     return sum;
 }
 
